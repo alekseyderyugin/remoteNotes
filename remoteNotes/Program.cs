add32a1a408f687e164771aa4acafdfc214f65b5
OzoneRPC <igfx@list.ru>
2015-11-04 19:24:15 +0300

    added Note class

--- remoteNotes/Program.cs
+++ remoteNotes/Program.cs
@@ -10,30 +10,29 @@ namespace remoteNotes
 	class MainClass
 	{
 		public static void Main(string[] args)
-		{
-			Application.Init();
-			MainWindow win = new MainWindow();
-			win.Show();
-			Application.Run();
+		{		
+				HttpClientChannel channel = new HttpClientChannel();
+				ChannelServices.RegisterChannel(channel);
 
-			HttpClientChannel channel = new HttpClientChannel();
-			ChannelServices.RegisterChannel(channel);
+				// Go get the remote object
+				object remoteObj = Activator.GetObject(
+					typeof(remoteNotesLib.MyClass),
+					"http://localhost:13101/myURI.soap"
+				);
 
-			// Go get the remote object
-			object remoteObj = Activator.GetObject(
-				typeof(remoteNotesLib.MyClass),
-				"http://localhost:13101/myURI.soap"
-			);
+				// Cast the returned proxy to the SimpleMath type
+				MyClass obj = (MyClass)remoteObj;
 
-			// Cast the returned proxy to the SimpleMath type
-			MyClass obj = (MyClass)remoteObj;
+				// Use the remote object
+				obj.getMessage("hui na na!");	
+				
+				Application.Init();
+				MainWindow win = new MainWindow();
+				win.Show();
+				Application.Run();
 
-			// Use the remote object
-			obj.getMessage("hui na na!");
-
-			// Ask user to press enter
-			Console.Write("Press enter to end");
-			Console.ReadLine();
-		}
+				
+				Console.ReadLine();
+			}
 	}
 }
---
+++ remoteNotesLib/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace remoteNotesLib
+{
+		public enum StateField
+		{
+				Added,
+				Updated,
+				Deleted,
+				NoChange
+		}
+		
+		public class Note
+		{
+				private int id;
+				private DateTime date;
+				public string title;
+				public string content;
+				private StateField StateField;
+
+				public Note()
+				{
+								this.id = base.GetHashCode();
+								this.StateField = StateField.NoChange;
+								this.date = DateTime.Now;
+
+				}
+		}
+}
+
